6ff2c16ba022c5dc7ddda6ace3559909d3fa5c95
Matt Anderson <matt@crossmarcsoftware.com>
2018-01-03 14:01:47 -0700

    Updated ProgramEntryPoint to use standard code alignment.

--- CodingStandards/ProgramEntryPoint.cs
+++ CodingStandards/ProgramEntryPoint.cs
@@ -7,31 +7,31 @@ using System.Threading.Tasks;
 
 class Program
 {
-        //This is the entry point for the program.
-        static int Main(string[] args)
-{
-    return writeMessage(dtNow, Console.Out);
-}
+    //This is the entry point for the program.
+    static int Main(string[] args)
+    {
+        return writeMessage(dtNow, Console.Out);
+    }
 
-        static DateTime dtNow = DateTime.Now;
+    static DateTime dtNow = DateTime.Now;
 
-public static int writeMessage(DateTime CurrentDate, TextWriter writer)
-{
-    var intRetVal = 0; const Int32 MESSAGE_COUNT = 5; for (int MyIndex = 0; MyIndex < MESSAGE_COUNT; MyIndex++)
-    /*********************/
-    /* greater than 2017 */
-    /*********************/
-    if (CurrentDate.Year > 2017) writer.WriteLine("Happy New Year!");
-    else
+    public static int writeMessage(DateTime CurrentDate, TextWriter writer)
+    {
+        var intRetVal = 0; const Int32 MESSAGE_COUNT = 5; for (int MyIndex = 0; MyIndex < MESSAGE_COUNT; MyIndex++)
+            /*********************/
+            /* greater than 2017 */
+            /*********************/
+            if (CurrentDate.Year > 2017) writer.WriteLine("Happy New Year!");
+            else
                 writer.WriteLine("Hello From " + CurrentDate.Year);
 
 
-    //--------------------return the result value----------------------------
+        //--------------------return the result value----------------------------
 
 
 
 
-    return intRetVal;
+        return intRetVal;
     }
 }
 
