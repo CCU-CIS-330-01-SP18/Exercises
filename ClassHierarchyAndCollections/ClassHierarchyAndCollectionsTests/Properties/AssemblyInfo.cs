83f436ddc1cdfa622d1f6cc5ff5f6345dc26b918
Ryan Hurley <ryan.hurley1212@gmail.com>
2018-01-30 18:58:12 -0700

    Started Assignment for Week 4 and created unit tests for Contact class.

---
+++ ClassHierarchyAndCollections/ClassHierarchyAndCollectionsTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClassHierarchyAndCollectionsTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClassHierarchyAndCollectionsTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d05af7dc-cac2-424f-aad1-67337cb749ae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
