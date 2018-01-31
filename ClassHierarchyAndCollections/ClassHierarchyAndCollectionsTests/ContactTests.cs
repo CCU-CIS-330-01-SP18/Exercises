1931d218763768ff07c9a8e3f9363a8574870d3c
Matt Anderson <matt@crossmarcsoftware.com>
2018-01-30 18:52:57 -0700

    Completed tests for Contact.

--- ClassHierarchyAndCollections/ClassHierarchyAndCollectionsTests/ContactTests.cs
+++ ClassHierarchyAndCollections/ClassHierarchyAndCollectionsTests/ContactTests.cs
@@ -11,6 +11,30 @@ namespace ClassHierarchyAndCollectionsTests
         public void CanCreateContact()
         {
             Contact c = new Contact();
+            Assert.IsNotNull(c);
+        }
+
+        [TestMethod]
+        public void ContactDerivesFromObject()
+        {
+            Contact c = new Contact();
+            Assert.IsInstanceOfType(c, typeof(Object));
+        }
+
+        [TestMethod]
+        public void ContactCanReadWriteDisplayName()
+        {
+            Contact c = new Contact();
+            c.DisplayName = "Matt";
+            Assert.AreEqual("Matt", c.DisplayName);
+        }
+
+        [TestMethod]
+        public void ContactCanReadWriteLegalName()
+        {
+            Contact c = new Contact();
+            c.LegalName = "Matt";
+            Assert.AreEqual("Matt", c.LegalName);
         }
     }
 }
