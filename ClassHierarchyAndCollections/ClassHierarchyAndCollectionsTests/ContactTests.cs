7018d970281cbb3344c6f182f76cff4d736f2046
Justin Horak <justinhorak1@gmail.com>
2018-01-30 17:43:25 -0800

    This is a test

---
+++ ClassHierarchyAndCollections/ClassHierarchyAndCollectionsTests/ContactTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ClassHierarchyAndCollections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassHierarchyAndCollectionsTests
+{
+    [TestClass]
+    public class ContactTests
+    {
+        [TestMethod]
+        public void CanCreateContact()
+        {
+            // 1. Add a reference, right click reference, projects.
+            // 2. Make Class accessable Go to class and make public.
+            // 3. Need using namespace for contact class.
+
+            Contact c = new Contact();
+
+        }
+    }
+}
