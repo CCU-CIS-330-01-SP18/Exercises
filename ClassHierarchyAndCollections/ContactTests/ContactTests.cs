58d3dc6b2bca76f84f09893806173aa5d98226b2
dee.randel <dee.randel@gmail.com>
2018-01-30 18:51:41 -0700

    Created Contact class and 4 tests for the class.

---
+++ ClassHierarchyAndCollections/ContactTests/ContactTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ClassHierarchyAndCollections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace ContactTests
+{
+    [TestClass]
+    public class ContactTests
+    {
+        [TestMethod]
+        public void CanCreateContact()
+        {
+
+            Contact c = new Contact();
+            Assert.IsNotNull(c);
+        }
+
+        [TestMethod]
+        public void ContactDerivesFromObject()
+        {
+            Contact c = new Contact();
+            Assert.IsInstanceOfType(c, typeof(Object));
+        }
+        [TestMethod]
+        public void CanReadWriteDisplayName()
+        {
+            Contact c = new Contact();
+            c.DisplayName = "Dean";
+            Assert.AreEqual("Dean", c.DisplayName);
+        }
+        [TestMethod]
+        public void CanReadWriteLegalName()
+        {
+            Contact c = new Contact();
+            c.LegalName = "test";
+            Assert.AreEqual("test", c.LegalName);
+        }
+    }
+}
