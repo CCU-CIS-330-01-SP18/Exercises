d88ece532604182e01cee4783c606ac369edc6a4
Matt Anderson <matt@crossmarcsoftware.com>
2018-01-30 18:42:59 -0700

    Added test project and started work on Contact tests.

---
+++ ClassHierarchyAndCollections/ClassHierarchyAndCollectionsTests/ContactTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ClassHierarchyAndCollections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassHierarchyAndCollectionsTests
+{
+    [TestClass]
+    public class ContactTests
+    {
+        [TestMethod]
+        public void CanCreateContact()
+        {
+            Contact c = new Contact();
+        }
+    }
+}
---
+++ ClassHierarchyAndCollections/ClassHierarchyAndCollectionsTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClassHierarchyAndCollectionsTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClassHierarchyAndCollectionsTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aa98eb45-6247-4ad6-b7c6-c23330b73863")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
