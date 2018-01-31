d6fabaa953307a8bc87ba2e49220f45c38dc5fc2
SpectralVulpine <spectralvulpine@outlook.com>
2018-01-30 18:43:01 -0700

    Set up all the basic classes
    No functionality exists in any of them except Contact as of yet.

---
+++ ClassHierarchyAndCollections/ClassHierarchyAndCollections/Contact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassHierarchyAndCollections
+{
+    /// <summary>
+    /// Represents a contact, with basic contact information.
+    /// </summary>
+    public class Contact
+    {
+        public string DisplayName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the Contact class, with the given information.
+        /// </summary>
+        /// <param name="displayName">The name by which the contact will be displayed.</param>
+        /// <param name="email">The contact's email address.</param>
+        /// <param name="phone">The contact's phone number.</param>
+        public Contact(string displayName, string email, string phone)
+        {
+            DisplayName = displayName;
+            EmailAddress = email;
+            PhoneNumber = phone;
+        }
+    }
+}
---
+++ ClassHierarchyAndCollections/ClassHierarchyAndCollectionsTest/ContactTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassHierarchyAndCollections;
+
+namespace ClassHierarchyAndCollectionsTests
+{
+    [TestClass]
+    public class ContactTests
+    {
+        [TestMethod]
+        public void CanCreateContact()
+        {
+            Contact contact = new Contact("John Doe", "jdoe@ccu.edu", "01189998819991197253");
+        }
+    }
+}
