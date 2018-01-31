da3baf233814f43cc618e0af194b170bb8ac0345
Lexey Janiszewski <Lexeyjmac@gmail.com>
2018-01-31 15:39:27 -0700

    Initial commit, Contact.cs and a few tests for Contact.cs

---
+++ ClassHierarchyAndCollections/ContactHierarchyTests/ContactTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassHierarchyAndCollections;
+
+namespace ContactHierarchyTests
+{
+    [TestClass]
+    public class ContactTest
+    {
+        [TestMethod]
+        public void CanCreateContact()
+        {
+            var createdContact = new Contact();
+            Assert.IsNotNull(createdContact);
+        }
+
+        [TestMethod]
+        public void ContactDerivesFromObject()
+        {
+            var createdContact = new Contact();
+            Assert.IsInstanceOfType(createdContact, typeof(Object));
+        }
+
+        [TestMethod]
+        public void CanReadWriteDisplayName()
+        {
+            var createdContact = new Contact();
+            createdContact.DisplayName = "Test";
+            Assert.AreEqual("Test", createdContact.DisplayName);
+        }
+
+        [TestMethod]
+        public void CanReadWriteLegalName()
+        {
+            var createdContact = new Contact();
+            createdContact.LegalName = "Test";
+            Assert.AreEqual("Test", createdContact.LegalName);
+        }
+    }
+}
---
+++ ClassHierarchyAndCollections/ContactHierarchyTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ContactHierarchyTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ContactHierarchyTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b67dd80-93b8-4ba2-8ef5-02744db85630")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
