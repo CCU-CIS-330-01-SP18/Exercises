f8e2059de4bd0ffd6805c848c221d4b10cbcdb6c
dee.randel <dee.randel@gmail.com>
2018-01-31 21:36:51 -0700

    Competed Individual class and its associated tests.

---
+++ ClassHierarchyAndCollections/ContactTests/IndividualTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassHierarchyAndCollections;
+
+namespace ContactTests
+{
+    [TestClass]
+    public class IndividualTests
+    {
+        [TestMethod]
+        public void CanCreateIndividual()
+        {
+            Individual i = new Individual();
+            Assert.IsNotNull(i);
+        }
+        [TestMethod]
+        public void IsDescendantOfContact()
+        {
+            Individual i = new Individual();
+            Assert.IsInstanceOfType(i, typeof(Contact));
+        }
+        [TestMethod]
+        public void CanReadWriteAge()
+        {
+            Individual i = new Individual();
+            i.Age = 22;
+            Assert.AreEqual(i.Age, 22);
+        }
+        [TestMethod]
+        public void CanReadWriteName()
+        {
+            Individual i = new Individual();
+            i.Name = "Dean";
+            Assert.AreEqual(i.Name, "Dean");
+        }
+    }
+}
