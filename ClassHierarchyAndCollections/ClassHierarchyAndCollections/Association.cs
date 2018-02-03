6584793f94e70be10b6e13614cc255edf3249f7b
Justin Horak <justinhorak1@gmail.com>
2018-02-03 11:20:23 -0800

    Did some More of the HW

---
+++ ClassHierarchyAndCollections/ClassHierarchyAndCollections/Association.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassHierarchyAndCollections
+{
+    class Association: Organization
+    {
+        public string AssociationName { get; set; }
+        public int AssociationMembers { get; set; }
+    }
+}
---
+++ ClassHierarchyAndCollections/ClassHierarchyAndCollections/Organization.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassHierarchyAndCollections
+{
+    class Organization: Contact
+    {
+        public string OrganizationName { get; set; }
+        public string OrganizaitonAddress { get; set; }
+    }
+}
