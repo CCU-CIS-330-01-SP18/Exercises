11147956887f36b64d085fd7e2d2eeda90037331
Chadder-Box <chaddparsons97@gmail.com>
2018-02-06 18:31:34 -0700

    Commitment Issues

--- ClassHierarchyAndCollections/ClassHierachyAndCollectionsTests/AssociationTests.cs
+++ ClassHierarchyAndCollections/ClassHierachyAndCollectionsTests/AssociationTests.cs
@@ -3,12 +3,67 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ClassHierachyAndCollectionsTests
 {
+    /// <summary>
+    /// Tests the functionality of the Association class.
+    /// </summary>
     [TestClass]
     public class AssociationTests
     {
+        /// <summary>
+        /// Tests to see if you can create an instance of the class.
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void CanCreateAssociation()
         {
+            var AssociationTests = new AssociationTests();
+            Assert.IsNotNull(AssociationTests);
+        }
+
+        /// <summary>
+        /// Tests if the class correctly inherets the first tier in the hierarchy.
+        /// </summary>
+        [TestMethod]
+        public void ContentDerivesFromOrganization()
+        {
+            AssociationTests a = new AssociationTests();
+            Assert.IsInstanceOfType(a, typeof(AssociationTests));
+        }
+
+        /// <summary>
+        /// Tests if the class correctly inherets the second tier in the hierarchy.
+        /// </summary>
+        [TestMethod]
+        public void ContentDerivesFromExtendedContent()
+        {
+            AssociationTests a = new AssociationTests();
+            Assert.IsInstanceOfType(a, typeof(AssociationTests));
+        }
+
+        /// <summary>
+        /// Tests the read/write capability of the first property.
+        /// </summary>
+        [TestMethod]
+        public void AssociationCanReadWriteName()
+        {        
+            /*
+            var createdAssociation = new Association();
+            createdBusiness.OwnerName = "Test";
+            Assert.AreEqual("Test", createdBusiness.OwnerName);
+            */
+        }
+
+        /// <summary>
+        /// Tests the read/write capability of the second property.
+        /// </summary>
+        [TestMethod]
+        public void AssociationCanReadWriteLocation()
+        {
+            /*
+            AssociationTests a = new AssociationTests();
+            a.State = "Colorado";
+            Assert.AreEqual("Colorado", a.State);
+            */
         }
     }
 }
+
--- ClassHierarchyAndCollections/ClassHierachyAndCollectionsTests/OrganizationTests.cs
+++ ClassHierarchyAndCollections/ClassHierachyAndCollectionsTests/OrganizationTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ClassHierachyAndCollectionsTests
 {
     [TestClass]
-    public class OrganizationTestscs
+    public class OrganizationTests
     {
         [TestMethod]
         public void TestMethod1()
