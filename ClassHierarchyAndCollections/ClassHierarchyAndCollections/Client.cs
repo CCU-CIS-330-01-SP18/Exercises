016b88cf9a9c1efa2cf186dcf87d2c6109b9809d
Macy Kasson <macykasson@yahoo.com>
2018-02-07 09:31:41 -0800

    Finished defining classes and properties

---
+++ ClassHierarchyAndCollections/ClassHierarchyAndCollections/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassHierarchyAndCollections
+{
+    class Client: Individual
+    {
+        public String ClientBusiness { get; set; }
+
+        public String ClientCreditStanding { get; set; }
+    }
+}
