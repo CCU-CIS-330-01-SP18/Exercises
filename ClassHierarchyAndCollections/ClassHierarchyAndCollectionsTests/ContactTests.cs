fe9c09c24fb98cb9a494affe075e3be5ae72aa6c
Macy Kasson <macykasson@yahoo.com>
2018-02-07 23:13:39 -0800

    Trying to set up my references? and it's making me angry...

--- ClassHierarchyAndCollections/ClassHierarchyAndCollectionsTests/ContactTests.cs
+++ ClassHierarchyAndCollections/ClassHierarchyAndCollectionsTests/ContactTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 
+
 namespace ClassHierarchyAndCollectionsTests
 {
     [TestClass]
--- ClassHierarchyAndCollections/ClassHierarchyAndCollectionsTests/IndividualTests.cs
+++ ClassHierarchyAndCollections/ClassHierarchyAndCollectionsTests/IndividualTests.cs
@@ -12,5 +12,21 @@ namespace ClassHierarchyAndCollectionsTests
             Individual i = new Individual();
             Assert.IsNotNull(i);
         }
+
+        [TestMethod]
+        public void CanGetSetPhoneNumber()
+        {
+            Individual i = new Individual();
+            i.PhoneNumber = "911";
+            Assert.AreEqual("911", i.PhoneNumber);
+        }
+
+        [TestMethod]
+        public void CanGetSetMaritalStatus()
+        {
+            Individual i = new Individual();
+            i.MaritalStatus = "It's Complicated";
+            Assert.AreEqual("It's Complicated", i.MaritalStatus);
+        }
     }
 }
