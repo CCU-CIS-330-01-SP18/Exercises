36df19eaa1cc0fa112383c32dd34036ae2f2dc9f
Macy Kasson <macykasson@yahoo.com>
2018-02-07 22:58:58 -0800

    Individual Test

---
+++ ClassHierarchyAndCollections/ClassHierarchyAndCollectionsTests/IndividualTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassHierarchyAndCollectionsTests
+{
+    [TestClass]
+    public class IndividualTests
+    {
+        [TestMethod]
+        public void CanMakeIndividual()
+        {
+            Individual i = new Individual();
+            Assert.IsNotNull(i);
+        }
+    }
+}
