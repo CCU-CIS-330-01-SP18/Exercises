af67befdf3c6f2a81361a76d467b63e84910cbf3
Macy Kasson <macykasson@yahoo.com>
2018-02-07 22:49:31 -0800

    First Test is jank

---
+++ ClassHierarchyAndCollections/ClassHierarchyAndCollectionsTests/ContactTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassHierarchyAndCollectionsTests
+{
+    [TestClass]
+    public class ContactTests
+    {
+        [TestMethod]
+        public void CanMakeContact()
+        {
+            Contact c = new Contact();
+            Assert.IsNotNull(c);
+        }
+
+        [TestMethod]
+        public void CanGetSetDisplayName()
+        {
+            Contact c = new Contact();
+            c.DisplayName = "Knuckles";
+            Assert.AreEqual("Knuckles", c.DisplayName);
+        }
+
+        [TestMethod]
+        public void CanGetSetEmail()
+        {
+            Contact c = new Contact();
+            c.EmailAddress = "swag@gmail.com";
+            Assert.AreEqual("swag@gmail.com", c.EmailAddress);
+        }
+    }
+    }
+}
