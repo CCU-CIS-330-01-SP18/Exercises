f8477f730d96b25c168020ada14085bfac407345
Ryan Hurley <ryan.hurley1212@gmail.com>
2018-02-13 19:02:20 -0700

    Started Week6 solution by creating the class library and creating the unit test.

---
+++ Week6/PhoneValidatorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PhoneValidatorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PhoneValidatorTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dc8c73f0-b011-408b-b52a-d294175e0c74")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Week6/Week6Tests/PhoneValidatorTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Week6;
+
+namespace Week6Tests
+{
+    [TestClass]
+    public class PhoneValidatorTests
+    {
+        [TestMethod]
+        public void PhoneValidatorReturnsFalseForInvalidPhone()
+        {
+            PhoneValidator.ValidatePhoneNumber("abc");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PhoneValidatorThrowsForNullPhone()
+        {
+            PhoneValidator.ValidatePhoneNumber(null);
+            /*ArgumentNullException thrownException = null;
+            try
+            {
+                PhoneValidator.ValidatePhoneNumber(null);
+            }
+            catch (ArgumentNullException ex)
+            {
+                thrownException = ex;
+            }
+
+            Assert.IsNotNull(thrownException);*/
+        }
+    }
+}
