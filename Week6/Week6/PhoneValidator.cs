2d7cce51c1350a86fe54ac92803d33e84def653e
Chadder-Box <chaddparsons97@gmail.com>
2018-02-14 09:40:02 -0700

    Added Test and methods.

---
+++ Week6/Week6/PhoneValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Week6
+{
+    public class PhoneValidator
+    {
+        public static  bool ValidatePhoneNumber(string phoneNumber)
+        {
+            if(phoneNumber == null)
+            {
+                throw new ArgumentNullException(nameof(phoneNumber));
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Week6/Week6Tests/PhoneValidatorTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Week6Tests
+{
+    [TestClass]
+    public class PhoneValidatorTests
+    {
+        [TestMethod]
+        public void PhoneValidatorReturnsFalseForInvalidPhoneNumber()
+        {
+            PhoneValidator.ValidatePhoneNumber("abc");
+        }
+
+        [TestMethod]
+        public void PhoneValidatorThrowsForNullPhone()
+        {
+            ArgumentNullException throwException = null;
+
+            try
+            {
+                PhoneValidator.ValidatePhoneNumber(null);
+            }
+            catch (ArguementOutOfRangeExeption ex)
+            {
+                throwException = ex;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PhoneValidatorThrowsNullPhone()
+        {
+            PhoneValidator.ValidatePhoneNumber("abc");
+        }
+    }
+}
