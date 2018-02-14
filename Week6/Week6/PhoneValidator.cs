4fc8bce23ca55e9c91c4c06737f8a7bd2a23b95b
Justin Horak <justinhorak1@gmail.com>
2018-02-13 18:52:54 -0700

    Initial Code Setup via Prof. Anderson.

---
+++ Week6/Week6/PhoneValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week6
+{
+    public static class PhoneValidator
+    {
+        public static bool ValidatePhoneNumber(string phoneNumber)
+        {
+            if(phoneNumber == null)
+            {
+                throw new ArgumentNullException(nameof(phoneNumber));
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Week6/Week6Tests/PhoneValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Week6;
+
+namespace Week6Tests
+{
+    [TestClass]
+    public class PhoneValidatorTests
+    {
+        [TestMethod]
+        public void PhoneValidatorCheck()
+        {
+            PhoneValidator.ValidatePhoneNumber("abc");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PhoneValidatorThrowsForNullPhone()
+        {
+            // Need to have ExpectedException Line 17:
+            PhoneValidator.ValidatePhoneNumber(null);
+
+            /* If you do not have ExpectedException need to write the following code below...
+            ArgumentNullException thrownException = null;
+            try
+            {
+                PhoneValidator.ValidatePhoneNumber(null);
+            }
+            catch(ArgumentNullException ex)
+            {
+                thrownException = ex;
+            }
+
+            Assert.IsNotNull(thrownException);
+            */
+        }
+    }
+}
