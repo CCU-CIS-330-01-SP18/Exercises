51489ef7ef1f6d3c5ef77ce94003eb96f1c71933
dee.randel <dee.randel@gmail.com>
2018-02-13 19:19:16 -0700

    created starter files and tests.

---
+++ Week6/Week6/PhoneValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week6
+{
+    public class PhoneValidator
+    {
+        public static bool ValidatePhoneNumber(string phoneNumber)
+        {
+            if (phoneNumber == null)
+            {
+                throw new ArgumentNullException(nameof(phoneNumber));
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Week6/week6Tests/PhoneValidatorTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Week6;
+namespace week6Tests
+{
+    [TestClass]
+    public class PhoneValidatorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PhoneValidatorThrowsForNullPhone()
+        {
+                PhoneValidator.ValidatePhoneNumber(null);
+        }
+    }
+}
