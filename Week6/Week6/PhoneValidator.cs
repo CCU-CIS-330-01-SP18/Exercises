bf35c6423f5524e04fe5f826946e52000c3ebc18
Ryan Hurley <ryan.hurley1212@gmail.com>
2018-02-18 14:56:42 -0700

    Made changes to my regex expression.

--- Week6/Week6/PhoneValidator.cs
+++ Week6/Week6/PhoneValidator.cs
@@ -13,7 +13,8 @@ namespace Week6
         //var phoneRegex = new Regex(@"([0-9]{3})[\s\.\-]([0-9]{3})[\s\.\-]([0-9]{4})|([0-9]{3})[\s\.\-]([0-9]{4})");
         public static bool ValidatePhoneNumber(string phoneNumber)
         {
-            var regex = new Regex(@"^([0-9]{3})[\s\.\-]([0-9]{3})[\s\.\-]([0-9]{4})|^([0-9]{3})[\s\.\-]([0-9]{4})");
+            //var regex = new Regex(@"^([0-9]{3})[\s\.\-]([0-9]{3})[\s\.\-]([0-9]{4})|^([0-9]{3})[\s\.\-]([0-9]{4})");
+            var regex = new Regex(@"^(\(?\d{3}\)?)[\s\.\-]?(\d{0,3})[\s\.\-]?(\d{0,4})$|^(\d{0,3})[\s\.\-]?(\d{0,4})$");
             Match match = regex.Match(phoneNumber);
 
             if (match.Success)
