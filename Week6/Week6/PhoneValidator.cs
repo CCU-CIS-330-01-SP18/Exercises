53ca6783e60d783b8b5fc752d3b12bf17d1332d5
dee.randel <dee.randel@gmail.com>
2018-02-18 19:35:25 -0700

    Added logic to PhoneValidator class and all tests. REMAINING: xml and code standards alignment

--- Week6/Week6/PhoneValidator.cs
+++ Week6/Week6/PhoneValidator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Week6
@@ -12,10 +13,39 @@ namespace Week6
         {
             if (phoneNumber == null)
             {
-                throw new ArgumentNullException(nameof(phoneNumber));
+                throw new ArgumentNullException(phoneNumber);
             }
-
-            return false;
+                
+            bool result;
+            if ((phoneNumber.Length >= 10) && (phoneNumber.Length <= 14))
+            {
+                result = TenDigitValidator(phoneNumber);
+                return result;
+            }
+            else if ((phoneNumber.Length <= 10) && (phoneNumber.Length >= 7))
+            {
+                result = SevenDigitValidator(phoneNumber);
+                return result;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(phoneNumber);
+            }
         }
+
+
+        private static bool TenDigitValidator(string phoneNumber)
+        {
+            var expression = new Regex(@"^\(?([0-9]{3})\)?[-.]?([0-9]{3})[-.]?([0-9]{4})$");
+            return expression.IsMatch(phoneNumber);
+        }
+
+        private static bool SevenDigitValidator(string phoneNumber)
+        {
+            var expression = new Regex(@"^\d?([0-9]{3})[-.]?([0-9]{4})$");
+            return expression.IsMatch(phoneNumber);
+        }
+
+
     }
 }
--- Week6/week6Tests/PhoneValidatorTests.cs
+++ Week6/week6Tests/PhoneValidatorTests.cs
@@ -7,15 +7,99 @@ namespace week6Tests
     public class PhoneValidatorTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TenDigitPhoneValidatorValidatesNoSpaces()
         {
+            bool result = false;
+            string phoneNumber = "8066837766";
+            result = PhoneValidator.ValidatePhoneNumber(phoneNumber);
+
+            Assert.IsTrue(result);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
+        public void TenDigitPhoneValidatorValidatesHyphens()
+        {
+            bool result = false;
+            string phoneNumber = "806-683-7766";
+            result = PhoneValidator.ValidatePhoneNumber(phoneNumber);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TenDigitPhoneValidatorValidatesPeriods()
+        {
+            bool result = false;
+            string phoneNumber = "806.683.7766";
+            result = PhoneValidator.ValidatePhoneNumber(phoneNumber);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TenDigitPhoneValidatorValidatesNoSpacesParentheses()
+        {
+            bool result = false;
+            string phoneNumber = "(806)6837766";
+            result = PhoneValidator.ValidatePhoneNumber(phoneNumber);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TenDigitPhoneValidatorValidatesHyphensParentheses()
+        {
+            bool result = false;
+            string phoneNumber = "(806)-683-7766";
+            result = PhoneValidator.ValidatePhoneNumber(phoneNumber);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void SevenDigitPhoneValidatorValidatesNoSpaces()
+        {
+            bool result = false;
+            string phoneNumber = "6837766";
+            result = PhoneValidator.ValidatePhoneNumber(phoneNumber);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void SevenDigitPhoneValidatorValidatesHyphen()
+        {
+            bool result = false;
+            string phoneNumber = "683-7766";
+            result = PhoneValidator.ValidatePhoneNumber(phoneNumber);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void SevenDigitPhoneValidatorValidatesPeriod()
+        {
+            bool result = false;
+            string phoneNumber = "683.7766";
+            result = PhoneValidator.ValidatePhoneNumber(phoneNumber);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
         public void PhoneValidatorThrowsForNullPhone()
         {
-                PhoneValidator.ValidatePhoneNumber(null);
+            PhoneValidator.ValidatePhoneNumber(null);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void PhoneValidatorThrowsForOutOfRangeNumber()
+        {
+            string phoneNumber = "(865)-9898-8989";
+            PhoneValidator.ValidatePhoneNumber(phoneNumber);
+        }
+
     }
 }
