b725c1503bdb32d823596b361ca6e0797c238277
xFyreWolfx <bukkit.firebreath15@gmail.com>
2018-02-20 21:19:53 -0700

    Finish assignment
    Woah dang, that regex is not easy on the eyes :eyes:

--- Regex/ValidationTests/PhoneValidatorTests.cs
+++ Regex/ValidationTests/PhoneValidatorTests.cs
@@ -10,7 +10,7 @@ namespace ValidationTests
         [TestMethod]
         public void CanValidateProperPhoneNumbers()
         {
-            // These tests should pass
+            // These tests should pass.
             Assert.IsTrue(PhoneValidator.ValidatePhoneNumber("(303) 963-3444"));
             Assert.IsTrue(PhoneValidator.ValidatePhoneNumber("+1 (303) 963-3444"));
             Assert.IsTrue(PhoneValidator.ValidatePhoneNumber("+1 (303) 963.3444"));
@@ -24,7 +24,7 @@ namespace ValidationTests
             Assert.IsTrue(PhoneValidator.ValidatePhoneNumber("963.3444"));
             Assert.IsTrue(PhoneValidator.ValidatePhoneNumber("963 3444"));
 
-            // These tests should be filtered out
+            // These tests should be filtered out.
             Assert.IsFalse(PhoneValidator.ValidatePhoneNumber("1-800 CALL-NOW"));
             Assert.IsFalse(PhoneValidator.ValidatePhoneNumber("1-800 GO-AWAY"));
             Assert.IsFalse(PhoneValidator.ValidatePhoneNumber("303.963.12345"));
@@ -46,6 +46,7 @@ namespace ValidationTests
         {
             // Call a method with invalid arguments. The call should fail on the following line.
             PhoneValidator.ValidatePhoneNumber("");
+            PhoneValidator.ValidatePhoneNumber(null);
 
             // Automatically fail this test if no exception was thrown.
             Assert.Fail();
