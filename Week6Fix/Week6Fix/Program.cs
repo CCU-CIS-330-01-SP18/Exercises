e368f0128affdcd68663398056dd0ff6b0b34b98
Justin Horak <justinhorak1@gmail.com>
2018-02-20 21:44:02 -0700

    Fix to Not RUnnin

---
+++ Week6Fix/Week6Fix/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Week6Fix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ValidatePhoneNumber("say 25 miles more");
+        }
+
+        public static bool ValidatePhoneNumber(string phoneNumber)
+        {
+            if (phoneNumber == null)
+            {
+                throw new ArgumentNullException(nameof(phoneNumber));
+            }
+
+            Console.WriteLine(Regex.Match(phoneNumber, @"(?<=say\s)\d+").Success);
+            System.Threading.Thread.Sleep(5000);
+
+            return false;
+        }
+    }
+}
