baf8a20d32715e6a6e880e9b1ef63369b93d1b8f
Ryan Hurley <ryan.hurley1212@gmail.com>
2018-02-24 16:04:15 -0700

    Created Week7 Solution and added Test and Class Library.

---
+++ Week7/Week7Tests/FileTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Week7;
+
+namespace Week7Tests
+{
+    [TestClass]
+    public class FileTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string content = @"c:\Data\Data.txt";
+            File file = new File();
+            file.FileName = content;
+
+            int contentLength = file.CountCharacters();
+
+            Assert.AreEqual(367, contentLength);
+            
+        }
+    }
+}
