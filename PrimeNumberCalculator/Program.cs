e9b2a306c0a1dc133ec0c2166d5a71762aff273a
dee.randel <dee.randel@gmail.com>
2018-03-01 15:52:12 -0700

    Created calculator class with functioning tread, tasks, and threadpool methods.

---
+++ PrimeNumberCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberCalculator
+{
+    /// <summary>
+    /// Dummy Class for the sake of solution building. 
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Dummy method for the sake of solution building.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
