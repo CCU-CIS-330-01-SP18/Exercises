13bbe2b9c64f39bdc096d848c05021f93c3b383a
xFyreWolfx <bukkit.firebreath15@gmail.com>
2018-03-01 09:58:38 -0700

    Added unit tests
    Oh snap I almost forgot unit tests :scream:

--- Week7Threading/Week7Threading/PageDownloader.cs
+++ Week7Threading/Week7Threading/PageDownloader.cs
@@ -11,13 +11,13 @@ namespace Week7Threading
     /// <summary>
     /// Downloads a list of webpages asynchronously, and calculate which download the fastest.
     /// </summary>
-    class PageDownloader
+    public class PageDownloader
     {
         /// <summary>
-        /// The entry point for the program.
+        /// The entry point for the program. Public method for testing purposes.
         /// </summary>
         /// <param name="args">Launch arguments (not used).</param>
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("== Welcome to Downloadifier! ==");
             HandleDownloads();
@@ -25,9 +25,9 @@ namespace Week7Threading
         }
 
         /// <summary>
-        /// Begins the process of downloading a list of websites, and calculates which downloaded fastest and slowest.
+        /// Begins the process of downloading a list of websites, and calculates which downloaded fastest and slowest. Public method for testing purposes.
         /// </summary>
-        private static void HandleDownloads()
+        public static void HandleDownloads()
         {
             string[] urls = new string[]
             {
@@ -38,7 +38,8 @@ namespace Week7Threading
                 "http://ccu.edu",
                 "http://amazon.com",
                 "http://github.com",
-                "http://crunchyroll.com"
+                "http://crunchyroll.com",
+                "thisisafakeurl"
             };
 
             List<Task> tasks = new List<Task>();
@@ -47,11 +48,13 @@ namespace Week7Threading
             foreach (string url in urls)
             {
                 tasks.Add(DownloadWebpage(url)
-                    .ContinueWith(t => pages.Add(url, t.Result)));
+                    .ContinueWith(t => addToDictionary(pages, url, t.Result)));
             }
 
             // Wait for all tasks to complete.
             Task.WaitAll(tasks.ToArray());
+
+            // Remove any URLs that are <0ms
 
             // Determine which websites were fastest / slowest.
             long slowestTime = pages.Values.Max();
@@ -65,21 +68,38 @@ namespace Week7Threading
         }
 
         /// <summary>
-        /// Performs the work of downloading a webpage and measuring the time elapsed.
+        /// Performs the work of downloading a webpage and measuring the time elapsed. Public method for testing purposes.
         /// </summary>
         /// <param name="url">The URL you wish to download.</param>
         /// <returns>The number of milliseconds elapsed during the download.</returns>
-        private static async Task<long> DownloadWebpage(string url)
+        public static async Task<long> DownloadWebpage(string url)
         {
-            Console.WriteLine($"Downloading \"{url}\"...");
-            Stopwatch sw = new Stopwatch();
-            HttpClient downloadClient = new HttpClient();
-            sw.Start();
-            string downloadContents = await downloadClient.GetStringAsync(url);
-            sw.Stop();
-            Console.WriteLine($"\"{url}\" was downloaded in {sw.ElapsedMilliseconds}ms!");
+            try
+            {
+                Console.WriteLine($"Downloading \"{url}\"...");
+                Stopwatch sw = new Stopwatch();
+                HttpClient downloadClient = new HttpClient();
+                sw.Start();
+                string downloadContents = await downloadClient.GetStringAsync(url);
+                sw.Stop();
+                Console.WriteLine($"\"{url}\" was downloaded in {sw.ElapsedMilliseconds}ms!");
 
-            return sw.ElapsedMilliseconds;
+                return sw.ElapsedMilliseconds;
+            }catch(Exception e)
+            {
+                Console.WriteLine($"There was an error downloading the URL \"{url}\". The program has skipped this URL. Reason: {e.GetType()}");
+
+                // Return a value less than 0 to indicate that there was a problem.
+                return -1;
+            }
+        }
+
+        public static void addToDictionary(Dictionary<string, long> d, string url, long result)
+        {
+            if(result > 0)
+            {
+                d.Add(url, result);
+            }
         }
     }
 }
---
+++ Week7Threading/Week7ThreadingTests/PageDownloaderTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Week7Threading;
+
+namespace Week7ThreadingTests
+{
+    /// <summary>
+    /// This program is very simple, so these tests are designed to ensure that the system has proper error checking.
+    /// </summary>
+    [TestClass]
+    public class PageDownloaderTests
+    {
+        [TestMethod]
+        public void CanDownloadWebpage()
+        {
+            Assert.IsNotNull(PageDownloader.DownloadWebpage("http://google.com"));
+        }
+
+        [TestMethod]
+        public void DoesNotAddBadValues()
+        {
+            Dictionary<string, long> testDictionary = new Dictionary<string, long>();
+            string testURL = "http://google.com";
+            long testDuration = -1;
+
+            PageDownloader.addToDictionary(testDictionary, testURL, testDuration);
+
+            Assert.IsFalse(testDictionary.ContainsKey(testURL));
+        }
+
+        [TestMethod]
+        public void DoesAddGoodValues()
+        {
+            Dictionary<string, long> testDictionary = new Dictionary<string, long>();
+            string testURL = "http://yahoo.com";
+            long testDuration = 1000;
+
+            PageDownloader.addToDictionary(testDictionary, testURL, testDuration);
+
+            Assert.IsTrue(testDictionary.ContainsKey(testURL));
+        }
+    }
+}
