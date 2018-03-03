78d6a4528f28b72fc3c3a5be293b7d484d427947
Ryan Hurley <ryan.hurley1212@gmail.com>
2018-03-03 14:31:23 -0700

    Added class hierarchy and created the IndividualList class.

---
+++ Week9CodingExercise/Week9CodingExercise/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Week9CodingExercise
+{
+    [Serializable]
+
+    [DataContract]
+    class Customer : Individual
+    {
+        public Customer(string name = null)
+            : base(name)
+        {
+        }
+    }
+}
---
+++ Week9CodingExercise/Week9CodingExercise/Individual.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week9CodingExercise
+{
+    public class Individual
+    {
+        private string gender;
+        public Individual(string name = null)
+        {
+            Name = name;
+        }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Gender
+        {
+            get
+            {
+                return gender;
+            }
+            set
+            {
+                gender.ToLower();
+                if (gender == "male" || gender == "female")
+                {
+                    gender = value;
+                }
+                else
+                {
+                    throw new FormatException("Entry Must Be Either 'Male' or 'Female', no other genders are recognized at this time.");
+                }
+            }
+        }
+
+        [DataMember]
+        public bool IsEmployee { get; set; }
+
+
+    }
+}
