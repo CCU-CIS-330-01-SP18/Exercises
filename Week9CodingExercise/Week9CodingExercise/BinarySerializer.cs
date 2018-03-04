a20a580bd3d89b3a2c851b3074381f237b46eb59
Ryan Hurley <ryan.hurley1212@gmail.com>
2018-03-04 13:51:11 -0700

    Added the BinarySerializer class to my solution.

---
+++ Week9CodingExercise/Week9CodingExercise/BinarySerializer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.IO;
+
+namespace Week9CodingExercise
+{
+    class BinarySerializer : ISerializer
+    {
+        static void BinarySerialization<T>(IndividualList<T> list) where T : Individual
+        {
+            var formatter = new BinaryFormatter();
+            
+            using (FileStream stream = File.Create("_b-individuals.txt"))
+            {
+                formatter.Serialize(stream, list);
+            }
+
+            IndividualList<T> deserializedList = null;
+            using (FileStream reader = File.OpenRead("_b-individuals.txt"))
+            {
+                deserializedList = formatter.Deserialize(reader) as IndividualList<T>;
+            }
+        }
+
+        public void Serialize(object o)
+        {
+
+        }
+
+        public void Deserialize(object o)
+        {
+
+        }
+    }
+}
