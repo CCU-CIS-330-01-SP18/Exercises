34e53c6fa0dc0b0ab017eed2d571c15e892823e4
Ryan Hurley <ryan.hurley1212@gmail.com>
2018-03-14 16:01:19 -0600

    Created the solution for my Week10 Exercise and added the functional classes.

---
+++ Week10NetworkingExercise/Week10NetworkingExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week10NetworkingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int portNumber;
+            bool validPortNumber = int.TryParse(args[0], out portNumber);
+
+            if (args.Length == 0)
+            {
+                throw new ArgumentNullException("No Command Line Argument Was Given");
+            }
+
+            WebServer.ListenForResponseAsync(portNumber);
+        }
+    }
+}
---
+++ Week10NetworkingExercise/Week10NetworkingExercise/WebServer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+using System.Net;
+using System.Net.Http;
+using System.IO;
+
+namespace Week10NetworkingExercise
+{
+    public class WebServer
+    {
+        public async static void ListenForResponseAsync(int portNumber)
+        {
+            UriBuilder uri = new UriBuilder("http", "localhost", portNumber);
+
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add(uri.ToString());
+            listener.Start();
+
+            while (true)
+            {
+                try
+                {
+                    var context = await listener.GetContextAsync();
+                }
+                catch (HttpListenerException) { break; }
+                catch (InvalidOperationException) { break; }
+            }
+
+            listener.Stop();
+        }
+
+        public static void ProcessRequestAsync(HttpListenerContext context)
+        {
+            string x = context.Request.QueryString["x"];
+            string y = context.Request.QueryString["y"];
+
+            int firstNumber;
+            int secondNumber;
+
+            bool xIsNumber = int.TryParse(x, out firstNumber);
+            bool yIsNumber = int.TryParse(y, out secondNumber);
+
+            if (xIsNumber || yIsNumber == false)
+            {
+                throw new InvalidOperationException("Both query strings must be integer values");
+            }
+
+            if (!string.IsNullOrWhiteSpace(x) && !string.IsNullOrWhiteSpace(y))
+            {
+                int result = firstNumber + secondNumber;
+
+                context.Response.ContentLength64 = Encoding.UTF8.GetByteCount(result.ToString());
+                context.Response.StatusCode = (int)HttpStatusCode.OK;
+
+                using (StreamWriter writer = new StreamWriter(context.Response.OutputStream))
+                {
+                    writer.Write(result);
+                }
+            }
+            else
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.Response.OutputStream.Close();
+            }
+        }
+    }
+}
