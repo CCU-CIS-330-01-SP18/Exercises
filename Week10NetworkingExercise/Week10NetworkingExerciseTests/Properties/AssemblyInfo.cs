bc6a8e0685dfe1aa407ceb97f56b5e0739130dc0
Ryan Hurley <ryan.hurley1212@gmail.com>
2018-03-18 11:59:49 -0600

    Added a UnitTests for my WebServer class.

---
+++ Week10NetworkingExercise/Week10NetworkingExerciseTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Week10NetworkingExerciseTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Week10NetworkingExerciseTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e957cf45-ec1e-4745-ac73-b4eca029ebd5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Week10NetworkingExercise/Week10NetworkingExerciseTests/WebServerTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Week10NetworkingExercise;
+
+namespace Week10NetworkingExerciseTests
+{
+    [TestClass]
+    public class WebServerTests
+    {
+        [TestMethod]
+        public void WebServerResponseOnRootPageIsOfTypeString()
+        {
+            WebServer.ListenForResponseAsync(3001);
+
+            string searchUrl = "http://localhost:3001/";
+
+            using (var client = new HttpClient())
+            {
+                Assert.AreEqual("<HTML><BODY> Hello, welcome to my server! Input some numbers into the query paramaters x and y to add some numbers together.</BODY></HTML>", client.GetStringAsync(searchUrl).Result);
+            }
+        }
+
+        [TestMethod]
+        public void WebServerRespondsWithCalculationWhenQueryParamatersAreSpecified()
+        {
+            WebServer.ListenForResponseAsync(3001);
+
+            string searchUrl = "http://localhost:3001/?x=5&y=4";
+
+            using (var client = new HttpClient())
+            {
+                Assert.AreEqual("9", client.GetStringAsync(searchUrl).Result);
+            }
+        }
+    }
+}
