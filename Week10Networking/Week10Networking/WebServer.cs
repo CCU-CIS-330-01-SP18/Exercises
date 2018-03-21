cfcdab738330377f860bdddb23a253e4ab11caef
SpectralVulpine <spectralvulpine@outlook.com>
2018-03-20 21:01:32 -0600

    Write basic functionality
    Untested as of yet.

--- Week10Networking/Week10Networking/WebServer.cs
+++ Week10Networking/Week10Networking/WebServer.cs
@@ -11,42 +11,103 @@ namespace Week10Networking
     /// <summary>
     /// A static class responsible for serving HTTP requests.
     /// </summary>
-    class WebServer
+    public class WebServer
     {
         /// <summary>
         /// The entry point of the program.
         /// </summary>
-        /// <param name="args">An array of arguments passed into the program upon execution.</param>
-        static void Main(string[] args)
+        /// <param name="args">An array of arguments passed into the program upon execution. The first index should be a valid port number.</param>
+        private static void Main(string[] args)
         {
-            int portNumber = 8001;
-            var uri = new UriBuilder("http", "localhost", portNumber);
-
-            while (true)
+            string invalidPortMessage = "Invalid port number supplied. Please supply a valid port number to listen on.";
+            if (args.Length < 1)
             {
-                // TODO: This code will open and close the listener over and over...there's got to be a better way
-                using (var listener = new HttpListener())
-                {
-                    listener.Prefixes.Add(uri.ToString());
-                    listener.Start();
+                Console.WriteLine("Please supply a port number to listen on.");
+                return;
+            }
+            try
+            {
+                RunWebListener(ParseArgument(args[0]));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(invalidPortMessage);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine(invalidPortMessage);
+            }
+            finally
+            {
+                Console.WriteLine("Exiting...");
+            }
+        }
 
-                    Console.WriteLine("Listening for requests on port " + portNumber.ToString());
-                    
+        /// <summary>
+        /// Takes the given argument and determines whether or not it is a valid port number, returning it in unsigned 16-bit integer (ushort) format if it is.
+        /// </summary>
+        /// <param name="argument">A raw argument in string format, passed into the program.</param>
+        /// <returns>A valid port number in ushort format.</returns>
+        public static ushort ParseArgument(string argument)
+        {
+            // Port numbers are unsigned 16-bit integers.
+            return Convert.ToUInt16(argument);
+        }
+
+        /// <summary>
+        /// Runs the web listener on the given port.
+        /// </summary>
+        /// <param name="portNumber">A valid port number to listen on.</param>
+        public static void RunWebListener(ushort portNumber)
+        {
+            var uri = new UriBuilder("http", "localhost", portNumber);
+            Console.WriteLine("Listening for requests on port " + portNumber.ToString());
+
+            using (var listener = new HttpListener())
+            {
+                listener.Prefixes.Add(uri.ToString());
+                listener.Start();
+                while (true)
+                {
                     var context = listener.GetContext();
                     var request = context.Request;
-                    
                     var response = context.Response;
-                    
-                    string responseString = "<HTML><BODY>CALCULATOR STUFF WILL GO HERE</BODY></HTML>";
+                    string responseString = "<html><body>";
+                    double? x = null;
+                    double? y = null;
+
+                    try
+                    {
+                        if (request.QueryString.HasKeys() &&
+                            request.QueryString.Get("x") != null &&
+                            request.QueryString.Get("y") != null)
+                        {
+                            x = Convert.ToDouble(request.QueryString.Get("x"));
+                            y = Convert.ToDouble(request.QueryString.Get("y"));
+                        }
+                    }
+                    catch (InvalidCastException) { }
+
+                    if (x == null || y == null)
+                    {
+                        responseString += "<p>Invalid query string entered. Please try again.</p>";
+                    }
+                    else
+                    {
+                        responseString += $"<p>{x} + {y} = {x + y}</p>";
+                    }
+
+                    responseString += "</body></html>";
                     byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-                    
+
                     response.ContentLength64 = buffer.Length;
                     var output = response.OutputStream;
                     output.Write(buffer, 0, buffer.Length);
-                    
+
                     output.Close();
-                    listener.Stop();
+                    break;
                 }
+                listener.Stop();
             }
         }
     }
---
+++ Week10Networking/Week10NetworkingTests/WebServerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Week10Networking;
+
+namespace Week10NetworkingTests
+{
+    [TestClass]
+    public class WebServerTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void HandlesOutOfRangePort()
+        {
+            string portNumber = "-7";
+            WebServer.ParseArgument(portNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void HandlesNonNumericPort()
+        {
+            string portNumber = "Not A Number";
+            WebServer.ParseArgument(portNumber);
+        }
+
+        [TestMethod]
+        public void DoesNotRespondToBadQueryString()
+        {
+            var uri = new UriBuilder("http", "localhost", 8080, "?x=ten&y=twenty");
+            WebServer.RunWebListener(8080);
+        }
+    }
+}
