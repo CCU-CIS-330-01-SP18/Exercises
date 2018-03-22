9e389130f8c185c21163c8935589988754b4bf9d
Macy Kasson <macykasson@yahoo.com>
2018-03-22 11:54:43 -0700

    It has begun.

---
+++ Week10Networking/Week10Networking/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Week10Networking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int portNumber = 0;
+            Console.WriteLine("Please Specify your desired port number:  ");
+            portNumber = Console.Read();
+        }
+    }
+}
