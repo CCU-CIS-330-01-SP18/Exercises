e9c0acb1bbee11584b97c27d0335caa5bc3c88cf
dee.randel <dee.randel@gmail.com>
2018-03-22 17:20:09 -0600

    Ran out of time, sadly.

---
+++ Week10Networking/Week10Networking/WebCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week10Networking
+{
+    /// <summary>
+    /// A class made in order to get 4/10 points.
+    /// </summary>
+    public class WebCalculator
+    {
+        /// <summary>
+        /// A method that prints "Hello World".
+        /// </summary>
+        /// <param name="args">User Input</param>
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
