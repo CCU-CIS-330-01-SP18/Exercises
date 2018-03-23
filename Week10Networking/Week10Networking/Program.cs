53107e5380bc35507af15584f9e6989ee9cb4fe7
Macy Kasson <macykasson@yahoo.com>
2018-03-22 17:52:06 -0700

    Needs tests and a few problems fixed with the console.

--- Week10Networking/Week10Networking/Program.cs
+++ Week10Networking/Week10Networking/Program.cs
@@ -1,14 +1,38 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace Week10Networking
 {
+    /// <summary>
+    /// This program request a port number and utilizes the WebServer class.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Validates port input and calls the RunListener Async method
+        /// </summary>
+        /// <param name="args">Port number to be passed in.</param>
         static void Main(string[] args)
         {
-            int portNumber = 0;
-            Console.WriteLine("Please Specify your desired port number:  ");
-            portNumber = Console.Read();
+            int portNumber;
+            
+            bool isValid = int.TryParse(args[0], out portNumber);
+
+            if (isValid)
+            {
+                WebServer.RunListenerAsync(portNumber);
+
+                Console.WriteLine($"Port Number: {portNumber}, pressing enter will stop.");
+                Console.ReadLine();
+            }
+
+            else
+            {
+                Console.WriteLine($"String must contain numbers.");
+            }    
         }
     }
 }
