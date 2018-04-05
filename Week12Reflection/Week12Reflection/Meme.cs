0f02499479656304301ce99083b1ed52607dd6ad
Macy Kasson <macykasson@yahoo.com>
2018-04-05 15:35:47 -0700

    Began project and made classes and test project

---
+++ Week12Reflection/Week12Reflection/Meme.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week12Reflection
+{
+    /// <summary>
+    /// Represents an instance of a Meme.
+    /// </summary>
+    public class Meme
+    {
+        /// <summary>
+        /// The title of the Meme.
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// Displays how funny the Meme is calculated on a scale from 1-10.
+        /// </summary>
+        public int HumorScore { get; set; }
+
+        /// <summary>
+        /// Makes the person who looks at the Meme, laugh.
+        /// </summary>
+        public void MakeMeLaugh() { }
+    }
+}
---
+++ Week12Reflection/Week12ReflectionTests/MemeTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Week12Reflection;
+
+namespace Week12ReflectionTests
+{
+    [TestClass]
+    public class MemeTests
+    {
+        [TestMethod]
+        public void CanConstructMeme()
+        {
+            var reflectionMeme = Activator.CreateInstance(typeof(Meme)) as Meme;
+
+            reflectionMeme.GetType
+        }
+    }
+}
