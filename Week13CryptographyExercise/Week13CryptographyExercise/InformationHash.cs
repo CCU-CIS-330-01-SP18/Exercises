7256dc47d480b2372a8040aae5fb597b96e1b71b
Ryan Hurley <ryan.hurley1212@gmail.com>
2018-04-05 12:53:02 -0600

    Created Project and Hashing class with a unit test as well.

---
+++ Week13CryptographyExercise/Week13CryptographyExercise/InformationHash.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week13CryptographyExercise
+{
+    public class InformationHash
+    {
+        public static byte[] Hash(string valuedInfo)
+        {
+            string password = valuedInfo;
+            byte[] data = Encoding.UTF8.GetBytes(password);
+            byte[] hash = SHA256.Create().ComputeHash(data);
+
+            //string hashedValue = Encoding.UTF8.GetString(hash);
+
+            return hash;
+        }
+    }
+}
---
+++ Week13CryptographyExercise/Week13CryptographyExerciseTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Week13CryptographyExerciseTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Week13CryptographyExerciseTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dcd04546-dd99-432f-acef-93a95e7b6e80")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
