4fc14335ff089c847d5b4af368e48230a4125eed
Dylan <Dylan@PC20>
2018-04-04 23:48:04 -0600

    Perhaps it's a bit morbid, but hey, that's the 14th century for ya ;)

---
+++ Week12Reflection/Reflecting/Prisoner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflecting
+{
+    /// <summary>
+    /// Posesses the 2 properties and 1 method relevant to an prisoner in the 14th century.
+    /// </summary>
+    public class Prisoner
+    {
+        private string LastWords { get; set; }
+        private bool IsCrying { get; set; }
+
+        /// <summary>
+        /// Kills the prisoner.
+        /// </summary>
+        private void Die()
+        {
+        }
+    }
+}
---
+++ Week12Reflection/ReflectingTest/PrisonerTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReflectingTest
+{
+    [TestClass]
+    public class PrisonerTest
+    {
+        [TestMethod]
+        public void CanConstructAndUsePropertiesAndMethodsForPrisoner()
+        {
+            // Confirm the class can be constructed.
+            var type = Type.GetType("Reflecting.Prisoner, Reflecting", true);
+            var instance = Activator.CreateInstance(type);
+
+            // Confirm that each property can be written and read.
+            var lastWordsProperty = instance.GetType().GetProperty("LastWords", BindingFlags.NonPublic | BindingFlags.Instance);
+            lastWordsProperty.SetValue(instance, "Dylan, you're one morbid SOB!", null);
+            var lastWordsValue = lastWordsProperty.GetValue(instance, null);
+
+            var isCryingProperty = instance.GetType().GetProperty("IsCrying", BindingFlags.NonPublic | BindingFlags.Instance);
+            isCryingProperty.SetValue(instance, true, null);
+            var isCryingValue = isCryingProperty.GetValue(instance, null);
+
+            // Confirm that each method can be invoked.
+            var dieMethod = instance.GetType().GetMethod("Die", BindingFlags.NonPublic | BindingFlags.Instance);
+            dieMethod.Invoke(instance, null);
+
+            Assert.IsNotNull(instance);
+            Assert.AreEqual(lastWordsValue, "Dylan, you're one morbid SOB!");
+            Assert.AreEqual(isCryingValue, true);
+        }
+    }
+}
