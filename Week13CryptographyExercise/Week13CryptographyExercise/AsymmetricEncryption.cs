66160378f2f595e8b5224e3dd911c7055547d0ba
Ryan Hurley <ryan.hurley1212@gmail.com>
2018-04-08 15:50:46 -0600

    Made a class for asymmetric encryption and made the unit test for it.

---
+++ Week13CryptographyExercise/Week13CryptographyExercise/AsymmetricEncryption.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week13CryptographyExercise
+{
+    public class AsymmetricEncryption
+    {
+        public static string Encrypt(string value)
+        {
+            string plainOldText = value;
+            string privateKey;
+            string publicKey;
+            byte[] encryptedValue;
+            string decryptedValue;
+
+            using (var csp = new RSACryptoServiceProvider())
+            {
+                publicKey = csp.ToXmlString(false);
+                privateKey = csp.ToXmlString(true);
+            }
+
+            using (var csp = new RSACryptoServiceProvider())
+            {
+                csp.FromXmlString(publicKey);
+
+                encryptedValue = csp.Encrypt(Encoding.UTF8.GetBytes(plainOldText), true);
+            }
+
+            using (var csp = new RSACryptoServiceProvider())
+            {
+                csp.FromXmlString(privateKey);
+
+                decryptedValue = Encoding.UTF8.GetString(csp.Decrypt(encryptedValue, true));
+            }
+
+            return decryptedValue;
+        }
+    }
+}
---
+++ Week13CryptographyExercise/Week13CryptographyExerciseTests/AsymmetricEncryptionTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Week13CryptographyExercise;
+
+namespace Week13CryptographyExerciseTests
+{
+    [TestClass]
+    public class AsymmetricEncryptionTests
+    {
+        [TestMethod]
+        public void CanDecryptEncryptedValue()
+        {
+            string text = "Some boring old text... yeah";
+
+            string decryptedText = AsymmetricEncryption.Encrypt(text);
+
+            Assert.AreEqual(text, decryptedText);
+        }
+    }
+}
