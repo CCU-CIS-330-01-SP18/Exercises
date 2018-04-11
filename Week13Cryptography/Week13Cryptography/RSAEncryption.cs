2f87c29016dea7384b55be0fc143e61b41f13228
xFyreWolfx <bukkit.firebreath15@gmail.com>
2018-04-10 23:16:22 -0600

    First trial run

---
+++ Week13Cryptography/Week13Cryptography/RSAEncryption.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week13Cryptography
+{
+    public class RSAEncryption
+    {
+        public string PublicKey { get; private set; }
+
+        public string PrivateKey { get; private set; }
+
+        public RSAEncryption()
+        {
+            using (RSACryptoServiceProvider crypto = new RSACryptoServiceProvider())
+            {
+                PublicKey = crypto.ToXmlString(false);
+                PrivateKey = crypto.ToXmlString(true);
+            }
+        }
+
+        public string Encrypt(string key, string plainText)
+        {
+            using (RSACryptoServiceProvider crypto = new RSACryptoServiceProvider())
+            {
+                crypto.FromXmlString(key);
+
+                return Encoding.UTF8.GetString(crypto.Encrypt(Encoding.UTF8.GetBytes(plainText), true));
+            }
+        }
+
+        public string Decrypt(string key, string encodedText)
+        {
+            using (RSACryptoServiceProvider crypto = new RSACryptoServiceProvider())
+            {
+                crypto.FromXmlString(key);
+
+                return Encoding.UTF8.GetString(crypto.Decrypt(Encoding.UTF8.GetBytes(encodedText), true));
+            }
+        }
+    }
+}
---
+++ Week13Cryptography/Week13Cryptography/SHAHashing.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week13Cryptography
+{
+    class SHAHashing
+    {
+        /*
+         * // Hash and "save" the password
+            string myPassword = "P@ssw0rd!";
+            byte[] data = Encoding.UTF8.GetBytes(myPassword);
+            byte[] hash = SHA256.Create().ComputeHash(data);
+            Console.WriteLine(Encoding.UTF8.GetString(hash));
+
+            // Confirm the user entered the same password.
+            byte[] data2 = Encoding.UTF8.GetBytes(myPassword);
+            byte[] hash2 = SHA256.Create().ComputeHash(data);
+            Console.WriteLine(Encoding.UTF8.GetString(hash2));
+
+            Console.WriteLine(hash.SequenceEqual(hash2));
+         */
+         public string Hash(string text)
+        {
+            return Encoding.UTF8.GetString(SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(text)));
+        }
+    }
+}
