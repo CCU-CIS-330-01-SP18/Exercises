cbb52817217567ffbebfa10498b7caabb866849d
dee.randel <dee.randel@gmail.com>
2018-04-12 16:38:26 -0600

    All done!

---
+++ Week13CryptographyExercise/Week13CryptographyExercise/AsymetricEncryption.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Week13CryptographyExercise
+{
+    /// <summary>
+    /// Asymetrically encrypts data.
+    /// </summary>
+    public class AsymetricEncryption
+    {
+        /// <summary>
+        /// Encrypts a string using a public and private key.
+        /// </summary>
+        /// <param name="plainText">String data to be encrypted.</param>
+        /// <returns>Encrypted data as type byte.</returns>
+        public static string EncryptData(string plainText)
+        {
+            byte[] encryptedData;
+            string decryptedData;
+            string privateKey;
+            string publicKey;
+
+            //Make public and private keys.
+            using (var rsaCryptoServiceProvider = new RSACryptoServiceProvider())
+            {
+                publicKey = rsaCryptoServiceProvider.ToXmlString(false);
+                privateKey = rsaCryptoServiceProvider.ToXmlString(true);
+            }
+
+            //Encrypt plainText asymetrically with the public key.
+            using (var rsaCryptoServiceProvider = new RSACryptoServiceProvider())
+            {
+                rsaCryptoServiceProvider.FromXmlString(publicKey);
+                encryptedData = rsaCryptoServiceProvider.Encrypt(Encoding.UTF8.GetBytes(plainText), true);
+            }
+
+            //Decrypt byte[] asmetrically with private key.
+            using (var rsaCryptoServiceProvider = new RSACryptoServiceProvider())
+            {
+                rsaCryptoServiceProvider.FromXmlString(privateKey);
+                decryptedData = Encoding.UTF8.GetString(rsaCryptoServiceProvider.Decrypt(encryptedData, true));
+            }
+
+            return decryptedData;
+        }
+    }
+}
---
+++ Week13CryptographyExercise/Week13CryptographyExercise/Hashing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Week13CryptographyExercise
+{
+    /// <summary>
+    /// Securely hashes a string of data.
+    /// </summary>
+    public class Hashing
+    {
+        /// <summary>
+        /// Generates a hash of an inputted string.
+        /// </summary>s
+        /// <param name="plainText">Data to be hashed.</param>
+        /// <returns>The resulting hashed data.s</returns>
+        public static byte[] HashData(string plainText)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(plainText);
+            byte[] hashData = SHA256.Create().ComputeHash(data);
+            
+            return hashData;
+        }
+    }
+}
