823b2ed962c6d49a69305f0694a08de5b8af6812
Chadder-Box <chaddparsons97@gmail.com>
2018-04-12 09:51:14 -0600

    Added tests and classes.

---
+++ Week13CryptographyExercise/Week13CryptographyExercise/AsymmetricEncryption.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace Week13CryptographyExercise
+{
+    /// <summary>
+    /// A class that provides a method to encrypt text asymmetrically.
+    /// </summary>
+    public static class AsymmetricEncryption
+    {
+        /// <summary>
+        /// Encrypts text asymmetrically.
+        /// </summary>
+        /// <param name="plainText">Text passed to be encrypted.</param>
+        /// <returns>An encrypted string.</returns>
+        public static string Encrypt(string plainText)
+        {
+            string decryptedValue;
+            string privateKey;
+            string publicKey;
+            byte[] encryptedValue;
+
+            // Gets public and private keys.
+            using (RSACryptoServiceProvider csp = new RSACryptoServiceProvider())
+            {
+                publicKey = csp.ToXmlString(false);
+                privateKey = csp.ToXmlString(true);
+            }
+
+            // Uses the public key to encrypt the value.
+            using (RSACryptoServiceProvider csp = new RSACryptoServiceProvider())
+            {
+                csp.FromXmlString(publicKey);
+
+                encryptedValue = csp.Encrypt(Encoding.UTF8.GetBytes(plainText), true);
+            }
+            
+            // Uses the private key to decrypt the value.
+            using (RSACryptoServiceProvider csp = new RSACryptoServiceProvider())
+            {
+                csp.FromXmlString(privateKey);
+
+                decryptedValue = Encoding.UTF8.GetString(csp.Decrypt(encryptedValue, true));
+            }
+            
+            return decryptedValue;
+        }
+    }
+}
---
+++ Week13CryptographyExercise/Week13CryptographyExercise/HashingEncryption.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week13CryptographyExercise
+{
+    /// <summary>
+    /// A class that provides a method to hash data.
+    /// </summary>
+    public static class HashingEncryption
+    {
+        /// <summary>
+        /// Takes a string, hashes it, then returns the byte[].
+        /// </summary>
+        /// <param name="password">A string passed to be hashed.</param>
+        /// <returns>The hashed value in a byte[] type format.</returns>
+        public static byte[] Hash(string password)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(password);
+            byte[] hash = SHA256.Create().ComputeHash(data);
+
+            return hash;
+        }
+    }
+}
