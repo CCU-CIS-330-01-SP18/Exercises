1939303c85ad225b909eb64aaaaad1c88a6a43c5
justinhorak <justinhorak1@gmail.com>
2018-04-11 19:56:41 -0600

    I think this may have worked
    Nothing was allowing my to push to github, CMD Nope, VS Nope, GitHub Desktop App Nope. So I manually uploaded the files to github if this did not work you can let me know

---
+++ CryptographyTests/AsymetricTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CryptographyAssignment;
+
+namespace CryptographyTests
+{
+    [TestClass]
+    public class AsymetricTests
+    {
+        [TestMethod]
+        public void AsymmetricCheck()
+        {
+            string text = "The Rocket will be launching in 30 days!!";
+
+            string decryptedText = Asymmetric.Encrypt(text);
+
+            Assert.AreEqual(text, decryptedText);
+        }
+    }
+}
