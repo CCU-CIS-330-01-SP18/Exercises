9d332e45df271010054d6ec33f1c4dd36a207fe8
Dylan <Dylan@PC20>
2018-04-18 14:45:04 -0600

    I have completed the assignment.

---
+++ Week14IDisposable/NeglectThoseDuties/NeglectDuties.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeglectThoseDuties
+{
+    /// <summary>
+    /// Neglects your duties so you feel less bad about neglecting your duties.
+    /// </summary>
+    public class NeglectDuties
+    {
+        /// <summary>
+        /// Prints a statment informing the user that the supplied duties have been neglected.
+        /// </summary>
+        /// <param name="duties">The duties to be neglected.</param>
+        /// <returns>Will return true once the neglected duties statement has been written.</returns>
+        public static bool NeglectMyDuties(string duties)
+        {
+            bool hasBeenNeglected = false;
+            Console.WriteLine("Your duty to '" + duties + "' has officially been neglected.");
+            hasBeenNeglected = true;
+            return hasBeenNeglected;
+        }
+    }
+}
