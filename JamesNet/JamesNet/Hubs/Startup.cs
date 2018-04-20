9c698ac636dccacff61a5dc11477d657eef55874
SpectralVulpine <spectralvulpine@outlook.com>
2018-04-20 13:42:28 -0600

    Remove irrelevant generated code

---
+++ JamesNet/JamesNet/Hubs/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(JamesNet.Hubs.Startup))]
+namespace JamesNet.Hubs
+{
+    /// <summary>
+    /// Startup class for SignalR.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Maps SignalR hubs to the given app stream.
+        /// </summary>
+        /// <param name="app">The app stream to map SignalR hubs to.</param>
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
