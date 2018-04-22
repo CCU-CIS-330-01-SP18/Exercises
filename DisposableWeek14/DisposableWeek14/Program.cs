90525c92b12c52b3e9fa8e19ac0b08678d731855
Macy Kasson <macykasson@yahoo.com>
2018-04-22 09:56:02 -0700

    help

--- DisposableWeek14/DisposableWeek14/Program.cs
+++ DisposableWeek14/DisposableWeek14/Program.cs
@@ -6,7 +6,10 @@ namespace DisposableWeek14
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("My computer is dying" +
+                "I am unable to complete this week's assignment" +
+                "I will explain further in github via comments, but currently i am under" +
+                "a large amount of duress. prayers plz");
         }
     }
 }
