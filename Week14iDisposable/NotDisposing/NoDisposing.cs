2c1de84f06587d9431734cf0a41cba3e071910be
dee.randel <dee.randel@gmail.com>
2018-04-22 14:25:49 -0600

    Im a slacker, I know :(

---
+++ Week14iDisposable/NotDisposing/NoDisposing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotDisposing
+{
+    /// <summary>
+    /// A class that definately does not implement iDisposable.
+    /// </summary>
+    public class NoDisposing
+    {
+        /// <summary>
+        /// Method that writes Hello World, and does not implement iDisposeble. 
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Week14iDisposable/NotDisposingTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NotDisposingTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NotDisposingTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bcd466f6-bfba-49cb-9fc2-695f1c1fcd3b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
