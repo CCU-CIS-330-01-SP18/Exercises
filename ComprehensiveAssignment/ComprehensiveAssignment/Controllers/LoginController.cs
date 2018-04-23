f916acba6143584170b530dc21200159a8c47f8b
Ryan Hurley <ryan.hurley1212@gmail.com>
2018-04-22 19:35:57 -0600

    Implemented Regex and form validation on login page.

--- ComprehensiveAssignment/ComprehensiveAssignment/Controllers/LoginController.cs
+++ ComprehensiveAssignment/ComprehensiveAssignment/Controllers/LoginController.cs
@@ -9,46 +9,62 @@ using ComprehensiveAssignment.Models;
 
 namespace ComprehensiveAssignment.Controllers
 {
+    /// <summary>
+    /// Controller that handles the login capabilities.
+    /// </summary>
     public class LoginController : Controller
     {
-        // GET: Login
+        /// <summary>
+        /// Displays the login page that is linked to the Login/index.cshtml View.
+        /// </summary>
+        /// <returns>The Login/index.cshtml View</returns>
         public ActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// Method responsible for creating a user.
+        /// </summary>
+        /// <param name="model">The passed in user from the form in index.cshtml.</param>
+        /// <returns>If model state is valid, logs them into the home page. If false,
+        /// it directs the user to correct their validation.</returns>
+        [HttpPost]
         public ActionResult CreateUser(User model)
         {
-            try
+            // If there is validation errors, reload the login view.
+            if (!ModelState.IsValid)
             {
-                var user = new User();
-                var hashedPassword = HashPassword(model.Password);
-
-                AdoTickrModel db = new AdoTickrModel();
-                
-                user.FirstName = model.FirstName;
-                user.LastName = model.LastName;
-                user.Email = model.Email;
-                //user.Password = model.Password;
-                user.Password = Encoding.UTF8.GetString(hashedPassword);
-                
-
-                db.Users.Add(user);
-
-                db.SaveChanges();
+                return RedirectToAction("Index", "Login");
             }
-            catch(Exception ex)
-            {
-                throw ex;
-            }
+
+            var user = new User();
+
+            // Value of hashing the passed in password to the controller.
+            var hashedPassword = HashPassword(model.Password);
+
+            AdoTickrModel db = new AdoTickrModel();
+
+            user.FirstName = model.FirstName;
+            user.LastName = model.LastName;
+            user.Email = model.Email;
             
+            // Turns the passed in byte[] to a string format.
+            user.Password = Encoding.UTF8.GetString(hashedPassword);
 
+            db.Users.Add(user);
 
+            db.SaveChanges();
+
+            // If validation was correct let them access the home page.
             return RedirectToAction("Index", "Home");
-            
-            
         }
 
+        /// <summary>
+        /// Method that turns a password into a hashed value.
+        /// </summary>
+        /// <param name="valuedInfo">The password passed in from the model.</param>
+        /// <returns>The hashed value in the form of a byte array.</returns>
         public static byte[] HashPassword(string valuedInfo)
         {
             string password = valuedInfo;
@@ -58,28 +74,35 @@ namespace ComprehensiveAssignment.Controllers
             return hash;
         }
 
+        /// <summary>
+        /// Method responsible for retrieving the user from the database.
+        /// </summary>
+        /// <param name="model">User model that is passed in from the form.</param>
+        /// <returns>Returns an ActionResult depending on whether a correct login was found.</returns>
         public ActionResult GetUser(User model)
         {
+            // If a user was not found, do not let them login.
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
             ActionResult action = null;
             AdoTickrModel db = new AdoTickrModel();
 
+            // Hash the password inputted.
             var hashedPassword = HashPassword(model.Password);
-            //List<User> users = new List<User>();
 
+            // Turn password into a string to match type that password is stored in the database.
             var retrievedPassword = Encoding.UTF8.GetString(hashedPassword);
-            /*var user = from i in db.Users
-                       where i.Email == model.Email && i.Password == model.Password
-                       select i;*/
 
+            // Find the user from the database where their email and password matched.
             var user = db.Users.Where(i => i.Email == model.Email && i.Password == retrievedPassword).FirstOrDefault();
-            //var users = db.Users.Select(u => new { u.Email, u.Password });
 
-
-
-
+            // If the credentials match whats in the database, log them in.
             if (model.Email == user.Email && user.Password == retrievedPassword)
             {
-                action = RedirectToAction("Index", "Login");
+                action = RedirectToAction("Index", "Home");
             }
 
             return action;
--- ComprehensiveAssignment/ComprehensiveAssignment/Models/User.cs
+++ ComprehensiveAssignment/ComprehensiveAssignment/Models/User.cs
@@ -6,22 +6,29 @@ namespace ComprehensiveAssignment.Models
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
 
+    /// <summary>
+    /// Responsible for creating a user based off ADO Model of my database table Users.
+    /// </summary>
     public partial class User
     {
         public int UserId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="First Name Cannot Be Empty Or Contain Numbers")]
         [StringLength(50)]
+        [RegularExpression(@"^([a-zA-Z]+)$")]
         public string FirstName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Last Name Cannot Be Empty Or Contain Numbers")]
         [StringLength(50)]
+        [RegularExpression(@"^([a-zA-Z]+)$")]
         public string LastName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Please enter a valid email... e.g. example@example.com")]
         [StringLength(100)]
+        [RegularExpression(@"^([a-z0-9\.\-]+)@([a-z0-9]+)[\.]([a-z]{3})$")]
         public string Email { get; set; }
 
+        //Reason StringLength is so high is because of the way hashed values are stored in my database.
         [Required]
         [StringLength(350)]
         public string Password { get; set; }
