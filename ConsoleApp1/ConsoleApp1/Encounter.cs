74a6ef1ac3764a80fd16a021e21494296678fa55
Macy Kasson <macykasson@yahoo.com>
2018-04-23 23:35:51 -0600

    Trying to set up some basic encounters
    I don't know how to write JSON (:
    We gonna learn it tho *cracks knuckles*

--- ConsoleApp1/ConsoleApp1/Encounter.cs
+++ ConsoleApp1/ConsoleApp1/Encounter.cs
@@ -21,9 +21,10 @@ namespace ConsoleApp1
         /// Available Actions that can be taken by the player in the Encounter.
         /// </summary>
         public List<Action> Actions { get; set; }
-        public Encounter(int encounterID,)
+        public Encounter(int encounterID, List<Entity> entities)
         {
-            int _encounterID = encounterID;
+            this.EncounterID = encounterID;
+            this.Entities = entities;
         }
     }
 }
