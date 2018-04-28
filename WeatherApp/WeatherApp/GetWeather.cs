3c994c75fce6989a107cf444af387056eafa798e
dee.randel <dee.randel@gmail.com>
2018-04-28 16:50:47 -0600

    Prelim commit. Almost Done.

---
+++ WeatherApp/WeatherApp/GetWeather.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace WeatherApp
+{
+    /// <summary>
+    /// A class retrieves weather data from an API and presents the data in a human readable format in an asynchronous manner. 
+    /// </summary>
+    public class GetWeather
+    {
+        /// <summary>
+        /// Asynchronously calls a weather API and reetrieves Json data.
+        /// </summary>
+        /// <returns>a RootObject object containing the resulting weather data.</returns>
+       public static async Task<RootObject> APIcall(string city)
+        {
+            var client = new RestClient("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=19021c4a1c8b014b72530857333b2019&mode");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("postman-token", "df4c4f90-a3b3-48a4-8de5-5b12ce307198");
+            request.AddHeader("cache-control", "no-cache");
+            var cancellationTokenSource = new CancellationTokenSource();
+            IRestResponse response  = await client.ExecuteTaskAsync(request, cancellationTokenSource.Token);
+
+            return JsonConvert.DeserializeObject<RootObject>(response.Content);
+        }
+
+        public static string GetCity()
+        {
+            Console.WriteLine("\r\nEnter the name of your city:");
+            var city = Console.ReadLine();
+            return city;
+        }
+    }
+}
---
+++ WeatherApp/WeatherApp/WeatherData.cs
@@ -0,0 +1,153 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    /// <summary>
+    /// A class that represents cooridnate data from JSON response.
+    /// </summary>
+    public class Coord
+    {
+        public double lon { get; set; }
+        public double lat { get; set; }
+    }
+
+    /// <summary>
+    /// A class that represents weather data from JSON response.
+    /// </summary>
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+
+    /// <summary>
+    /// A class that represents the main data from JSON response.
+    /// </summary>
+    public class Main
+    {
+        public double temp { get; set; }
+        public int pressure { get; set; }
+        public int humidity { get; set; }
+        public double temp_min { get; set; }
+        public double temp_max { get; set; }
+    }
+
+    /// <summary>
+    /// A class that represents wind data from JSON response.
+    /// </summary>
+    public class Wind
+    {
+        public double speed { get; set; }
+        public int deg { get; set; }
+    }
+
+    /// <summary>
+    /// A class that represents cloud data from JSON response.
+    /// </summary>
+    public class Clouds
+    {
+        public int all { get; set; }
+    }
+
+    /// <summary>
+    /// A class that represents system data from JSON response.
+    /// </summary>
+    public class Sys
+    {
+        public int type { get; set; }
+        public int id { get; set; }
+        public double message { get; set; }
+        public string country { get; set; }
+        public int sunrise { get; set; }
+        public int sunset { get; set; }
+    }
+
+    /// <summary>
+    /// A class that represents the Json response from the API call. 
+    /// </summary>
+    public class RootObject
+    {
+        public Coord coord { get; set; }
+        public List<Weather> weather { get; set; }
+        public string @base { get; set; }
+        public Main main { get; set; }
+        public int visibility { get; set; }
+        public Wind wind { get; set; }
+        public Clouds clouds { get; set; }
+        public int dt { get; set; }
+        public Sys sys { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public int cod { get; set; }
+
+        /// <summary>
+        /// Generates weather report from Json response.
+        /// </summary>
+        public bool GenerateReport()
+        {
+            try
+            {
+                Console.WriteLine("**********Your Weather Report**********");
+                Console.WriteLine("In " + name + ":");
+                Console.WriteLine("    Weather: " + weather[0].description);
+                Console.WriteLine("    Current Temperature: " + ((9 / 5) * (main.temp - 273) + 32) + " Degrees Fahrenheit.");
+                Console.WriteLine("    Humidity: " + main.humidity + "%");
+                Console.WriteLine("    Wind Speed: " + wind.speed + " Miles per Hour.");
+                Console.WriteLine("\r\n----Clothing Suggestions----");
+                GetSuggestions();
+                return true;
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("Failed to retrieve weater data: Check city name spelling. \r\n" + e);
+                Console.WriteLine("Press ENTER to retry.");
+                return false;
+            }
+            
+        }
+
+        /// <summary>
+        /// Selects clothing suggestions to add to the weather report, depending on weather conditions. 
+        /// </summary>
+        public void GetSuggestions()
+        {
+            if (((9 / 5) * (main.temp - 273) + 32) <= 45)
+            {
+                Console.WriteLine("Looks Like its pretty chilly! Grab a jacket!");
+            }
+            else if (((9 / 5) * (main.temp - 273) + 32) > 45 && ((9 / 5) * (main.temp - 273) + 32) <= 70)
+            {
+                Console.WriteLine("Balmy weather today, Go for the T-shirt");
+            }
+            else
+            {
+                Console.WriteLine("Its going to be hot! Shorts are advised.");
+            }
+
+            if (wind.speed >= 15 && wind.speed <= 25)
+            {
+                Console.WriteLine("Its breezy out there, maybe a pull over?");
+            }
+            else if (wind.speed < 15)
+            {
+                Console.WriteLine("The wind is still today!");
+            }
+            else
+            {
+                Console.WriteLine("Hold onto your hat! Its a windy day! Grab a wind breaker.");
+            }
+
+            Console.WriteLine("\r\nThats all from me! See ya next time!");
+            Console.WriteLine("Press Enter to quit.");
+            Console.ReadLine();
+        }
+    }
+}
