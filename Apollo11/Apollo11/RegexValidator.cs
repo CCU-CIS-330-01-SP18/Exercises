7830212c3e65eb0f4e5dc3e5ae4aa90b5c420768
Justin Horak <justinhorak1@gmail.com>
2018-04-30 10:30:13 -0600

    Commit 1

---
+++ Apollo11/Apollo11/RegexValidator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Apollo11
+{
+    public class RegexValidator
+    {
+        public static bool EmailValidator(string emailAddress)
+        {
+            bool isEmailValid = Regex.IsMatch(emailAddress, @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})$");
+
+            if (isEmailValid)
+            {
+                return true;
+            }
+            else if (string.IsNullOrEmpty(emailAddress))
+            {
+                throw new ArgumentNullException();
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+
+
+
+    }
+}
+
+
---
+++ Apollo11/Apollo11/SpaceHeadquarters.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apollo11
+{
+    class SpaceHeadquarters
+    {
+        static void Main(string[] args)
+        {
+            BeginSpaceTravel();
+        }
+
+
+        public static void BeginSpaceTravel()
+        {
+            Console.WriteLine("Welcome to Apollo 11 Space Travel.");
+            Console.WriteLine("Enter your email to begin");
+            string email = Console.ReadLine();
+
+            try
+            {
+                bool emailSuccess = RegexValidator.EmailValidator(email);
+                if (emailSuccess)
+                {
+                    Console.WriteLine("Success");
+                    Console.ReadLine();
+                }
+            }
+            catch(ArgumentNullException error)
+            {
+                Console.WriteLine($"{error.GetType().Name}: You did not enter an Email Address.");
+            }
+            catch(ArgumentOutOfRangeException error)
+            {
+                Console.WriteLine($"{error.GetType().Name}: You entered an invalid email. ");
+            }
+        }
+    }
+}
